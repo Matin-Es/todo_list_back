326225082e2868642230b697bc18c876f7532e53
Matin Esmaili <esmaili.matin82@gmail.com>
2022-08-11 19:51:10 +0430

    changed static file locati

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -130,9 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
