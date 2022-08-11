4cc62b4ba3ed04d54c92c07e7241db77dc0ba341
Matin Esmaili <esmaili.matin82@gmail.com>
2022-08-11 19:29:53 +0430

    added loggins for sv err 500

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -140,3 +140,18 @@ django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
